93b2284adf6d9d54fe4d29b014a68ca0a2034fa2
Mohamed Hegazy <m.hegazy94@hotmail.com>
2018-11-02 17:33:29 +0100

    Initial netflix plus commit

---
+++ src/HeroButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class HeroButton extends React.Component {
+  render() {
+    return (
+      <a href="#" className="Button" data-primary={this.props.primary}>{this.props.text}</a>
+    );
+  }
+}
+
+export default HeroButton;
---
+++ src/UserProfile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class UserProfile extends React.Component {
+  render() {
+    return (
+      <div className="UserProfile">
+        <div className="User">
+          <div className="name">Mohamed Hegazi</div>
+          <div className="image"><img src="" alt="profile" /></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
